5ee10619c2d49191fed5b4c775795cb51e53ba1d
NatasaZupanski <nzitune@gmail.com>
2021-04-09 14:13:55 -0400

    Started programming ColonyView

--- Shared/View/MainViews/ColonyView.swift
+++ Shared/View/MainViews/ColonyView.swift
@@ -11,7 +11,12 @@ struct ColonyView: View {
     var colony : Colony
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            /*ForEach(colony.aliveCells) {
+                Text("")
+            }*/
+            Text("Testing")
+        }
     }
 }
 
