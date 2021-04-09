c3275d4e804bc55860b170f5c80c1bf89ef26ad9
NatasaZupanski <nzitune@gmail.com>
2021-04-09 13:50:56 -0400

    Added Colony View

---
+++ Shared/View/MainViews/ColonyView.swift
@@ -0,0 +1,20 @@
+//
+//  ColonyView.swift
+//  Conways_iPad (iOS)
+//
+//  Created by Natasa Zupanski on 4/9/21.
+//
+
+import SwiftUI
+
+struct ColonyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColonyView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColonyView()
+    }
+}
