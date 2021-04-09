0afa30110afd0eb18f4cc21a8d3013578613579c
NatasaZupanski <nzitune@gmail.com>
2021-04-09 13:19:25 -0400

    Initial Commit

---
+++ Shared/Conways_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conways_iPadApp.swift
+//  Shared
+//
+//  Created by Natasa Zupanski on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Conways_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
