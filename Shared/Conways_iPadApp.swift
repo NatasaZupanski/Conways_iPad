6635c1fa789e0f05573067c8ad15069dfd3bf7d3
NatasaZupanski <nzitune@gmail.com>
2021-04-09 22:13:42 -0400

    Tested Tap Gesture through simulator; commented out now

--- Shared/Conways_iPadApp.swift
+++ Shared/Conways_iPadApp.swift
@@ -12,6 +12,12 @@ struct Conways_iPadApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+            /*ColonyView(colony: {
+                var colony = Colony(size: 60)
+                colony.setCellAlive(row: 1, col: 1)
+                colony.setCellAlive(row: 2, col: 2)
+                return colony
+            }())*/
         }
     }
 }
