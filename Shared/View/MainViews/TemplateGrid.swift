86fee7aa24e0f8c6da7a68c0acb17d9f565eb454
NatasaZupanski <nzitune@gmail.com>
2021-04-10 14:53:17 -0400

    First attempt at Template Grid

---
+++ Shared/View/MainViews/TemplateGrid.swift
@@ -0,0 +1,50 @@
+//
+//  TemplateGrid.swift
+//  Conways_iPad (iOS)
+//
+//  Created by Natasa Zupanski on 4/10/21.
+//
+
+import SwiftUI
+
+struct TemplateGrid: View {
+    var templates : [Colony]
+    var parsedTemplates : [(Colony, Colony?)] { // error here with amount of templates returned
+        var parsed = [(Colony, Colony?)]()
+        var count = 0
+        var partial : (Colony, Colony?) = (Colony(size: 60), nil)
+        for template in templates {
+            if count == 0 {
+                partial.0 = template
+                count += 1
+            }
+            if count == 1 {
+                partial.1 = template
+                count = 0
+                parsed += [partial]
+            }
+            parsed += [partial]
+        }
+        return parsed
+    }
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            ForEach(parsedTemplates, id: \.self.0.id) { tuple in
+                HStack {
+                    Template(colony: tuple.0, width: 200.0)
+                    if tuple.1 != nil {
+                        Template(colony: tuple.1!, width: 200.0)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct TemplateGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        TemplateGrid(templates: [Colony(size: 60), Colony(size: 60), Colony(size: 60)])
+    }
+}
