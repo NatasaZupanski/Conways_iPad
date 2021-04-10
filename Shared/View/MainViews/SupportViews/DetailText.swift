c807d4346352e1b864f649e23d3af94df2fc6962
NatasaZupanski <nzitune@gmail.com>
2021-04-10 14:21:21 -0400

    Added SupportViews folder and DetailText view to be added in the controls

---
+++ Shared/View/MainViews/SupportViews/DetailText.swift
@@ -0,0 +1,20 @@
+//
+//  DetailText.swift
+//  Conways_iPad (iOS)
+//
+//  Created by Natasa Zupanski on 4/10/21.
+//
+
+import SwiftUI
+
+struct DetailText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailText_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailText()
+    }
+}
