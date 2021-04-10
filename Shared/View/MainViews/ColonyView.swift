73355299cd6279fb73c2be52c08a8661609faec3
NatasaZupanski <nzitune@gmail.com>
2021-04-09 21:59:09 -0400

    Base of Colony View established; will probably need/want to change some properties of Colony next

--- Shared/View/MainViews/ColonyView.swift
+++ Shared/View/MainViews/ColonyView.swift
@@ -9,27 +9,37 @@ import SwiftUI
 
 struct ColonyView: View {
     var colony : Colony
+    let name : String
+    var generationNumber : Int
     
     var body: some View {
-        ZStack {
-            ForEach(0..<colony.livingCells().count) { _ in
-                Text("hi")
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(0..<colony.livingCells().count) { index in
+                    CellView(size: Double(geometry.size.height)/60.0)
+                        .offset(x: CGFloat(Double(colony.livingCells()[index].col)*Double(geometry.size.height)/(60.0)),
+                                y: CGFloat(Double( colony.livingCells()[index].row)*Double(geometry.size.height)/(60.0)))
+                }
+                //Text("Testing")
             }
-            //Text("Testing")
-        }
+        }.navigationBarTitle({
+            return Text(name) + Text("Generation: ") + Text(String(generationNumber))
+        }())
     }
 }
 
 struct ColonyView_Previews: PreviewProvider {
     static var previews: some View {
-        ColonyView(colony: Colony(size: 60))
+        ColonyView(colony: Colony(size: 60), name: "Unnamed Colony", generationNumber: 2)
     }
 }
 
 struct CellView : View {
     let size : Double
+    //let alive : Bool
     
     var body: some View {
         RoundedRectangle(cornerRadius: CGFloat(size / 10.0))
+            .foregroundColor(.blue)
     }
 }
